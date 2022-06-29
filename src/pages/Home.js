f557d57a825b8c4d2beb196ae24497ccf8d1f3ed
Eliseo Diaz Medina <eliseodm@gmail.com>
2022-06-29 17:33:12 -0300

    feature/2: Implement read users and list on a Table. Pagination on the client side using an Antd component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios'
 import { Form } from '../components/Form'
+import { Table, TableComponent } from '../components/Table'
 import './Home.css'
 
 export const Home = () => {
+  const [usersList, setUsersList] = useState([]);
+
+  useEffect(() => {
+  
+  Axios.get('http://localhost:3001/readUsers').then((response) => {
+      setUsersList(response.data)
+    })
+  }, [])
+  
+    
+  
+
   return (
     <div className='Home'>
-        <div className='title'>Lista de Usuarios</div>
+        <div className='title'>Usuarios</div>
         <Form />
-
+        <TableComponent data={usersList} />
+        {/* <button onClick={getAllUsers}>Listar Usuarios</button> */}
     </div>
   )
 }
